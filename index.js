861a29fea0e4cd05d0dd5eb62571382a371282b2
DinislamAdin <dinislam.adin20045@gmail.com>
2023-05-31 10:09:40 +0600

    jwt is added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
@@ -36,6 +37,15 @@ async function run() {
         const cartCollection = client.db("bistroDB").collection("carts");
 
 
+        // jwt
+
+        app.post("/jwt", async(req, res)=>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({token})
+        })
+
         // user related api
 
         app.get("/users", async(req, res)=>{
@@ -51,6 +61,18 @@ async function run() {
                 return res.send({message: "user already exist"})
             }
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.patch("/users/admin/:id", async(req, res)=>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updatedDoc ={
+                $set:{
+                    role: 'admin'
+                }
+            };
+            const result = await usersCollection.updateOne(filter, updatedDoc);
             res.send(result);
         })
 
